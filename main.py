b98ef58c305567172538ad7dd93b1587cb03bfd9
EdPeReg <dardo1818@gmali.com>
2025-02-08 20:28:57 -0600

    #2: Fix page duplication for md file

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 from datetime import datetime
 
 from pathlib import Path
+from typing import TextIO
 from pdf import PDF
 
 logging.basicConfig(
@@ -17,6 +18,19 @@ def create_folder(path: Path):
     if not path.exists():
         logging.info(f"Folder '{path}' doesn't exist, creating folder")
         path.mkdir(parents=True, exist_ok=True)
+
+def page_is_duplicated(file: TextIO, text: str) -> bool:
+    """Check if the line 'Page: X' is already in the text
+
+    Args:
+        file (TextIO): A file already open to be processed
+        text (str): Text where duplicates are found
+
+    Returns:
+        bool: True if the 'Page: X' is duplocated, False othewise.
+    """
+    page_lines = {line.strip() for line in file if line.startswith("Page: ")}
+    return any(page in text for page in page_lines)
 
 def write_file(file: Path, text: str, page_no: int):
     """Create a new file if it doesn't exist or append the text to an existing file"""
@@ -32,9 +46,13 @@ def write_file(file: Path, text: str, page_no: int):
 
         file.write_text(final_text)
     else:
-        logging.info(f"Markdown file exist, using existing file '{file}' with PDF page {page_no}")
-        with open(file, mode="a", encoding="utf-8") as f:
-            f.write(text)
+        with open(file, mode="r+", encoding="utf-8") as f:
+            duplicated = page_is_duplicated(f, text)
+            if duplicated:
+                logging.info(f"Page {page_no} already exist. Skipping.")
+            else:
+                logging.info(f"Markdown file exist, using existing file '{file}' with PDF page {page_no}")
+                f.write(text)
 
 def main():
     if len(sys.argv) < 5:
